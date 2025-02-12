1a7ae42bf53afed3be5b17e60945fadca0b0e966
Kimsreng28 <kimsrenghai@gmail.com>
2025-02-12 09:31:34 +0700

    New Code Start

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>App SSP</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
